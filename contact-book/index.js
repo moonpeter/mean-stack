3de6717645a8d0da946ca67ecd187a0aaf874d58
moonpeter <moonpeter@kakao.com>
2020-06-23 00:11:06 +0900

    update

--- contact-book/index.js
+++ contact-book/index.js
@@ -25,14 +25,18 @@ app.set('view engine', 'ejs');
 app.use(express.static(__dirname+'/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(methodOverride('_method'));
+app.use(methodOverride('_method'))
 
-//Routes
-app.use('/', require('./routes/home'));
-app.use('/contacts', require('./routes/contacts'));
+// Routes
+app.use('/', require('./routes/home')); // 1
+app.use('/contacts', require('./routes/contacts')); // 2
 
 // Port setting
 var port = 3000;
 app.listen(port, function(){
   console.log('server on! http://localhost:'+port);
 });
+
+
+
+// mongoose.connect("mongodb+srv://test_username:test_password@cluster0-tnv2b.mongodb.net/test?retryWrites=true&w=majority");
